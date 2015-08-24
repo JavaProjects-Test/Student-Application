1764e0b29f16bc81d8ac942f88a59bb009152ecf
kaushiksahil <kaushiksahil06@gmail.com>
2015-08-24 14:54:46 +0530

    Loop Issue Solve

--- Student/src/main/java/com/student/client/StudentMain.java
+++ Student/src/main/java/com/student/client/StudentMain.java
@@ -18,61 +18,63 @@ public class StudentMain {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Student objectStudent = new Student();
 		IStudentService objectService = new StudentServiceImpl();
-		System.out.println("Enter Your Choice: ");
-		System.out.println("1. Add new Student");
-		System.out.println("2. Print all Students");
+		while (true) {
+			System.out.println("Enter Your Choice: ");
+			System.out.println("1. Add new Student");
+			System.out.println("2. Print all Students");
 
-		try {
-			switch (Integer.parseInt(br.readLine())) {
+			try {
+				switch (Integer.parseInt(br.readLine())) {
 
-			case 1: {
-				System.out.println("Enter Roll Number: ");
-				objectStudent.setRollNumber(br.readLine());
-				System.out.println("Enter First Name: ");
-				objectStudent.setFirstName(br.readLine());
-				System.out.println("Enter Last Name: ");
-				objectStudent.setLastName(br.readLine());
-				System.out.println("Enter Branch: ");
-				objectStudent.setBranch(br.readLine());
+				case 1: {
+					System.out.println("Enter Roll Number: ");
+					objectStudent.setRollNumber(br.readLine());
+					System.out.println("Enter First Name: ");
+					objectStudent.setFirstName(br.readLine());
+					System.out.println("Enter Last Name: ");
+					objectStudent.setLastName(br.readLine());
+					System.out.println("Enter Branch: ");
+					objectStudent.setBranch(br.readLine());
 
-				Queue<Student> queue = new ArrayDeque<Student>();
-				queue.add(objectStudent);
-				
-				objectService.addNewStudent(queue);
-				break;
-			}
+					Queue<Student> queue = new ArrayDeque<Student>();
+					queue.add(objectStudent);
 
-			case 2: {
-				List<Student> list = objectService.printStudent();
-				Iterator<Student> itr = list.iterator();
-				while (itr.hasNext()) {
-
-					Student objStudent = itr.next();
-					System.out.println("Roll Number: "
-							+ objStudent.getRollNumber());
-					System.out.println("First Name: "
-							+ objStudent.getFirstName());
-					System.out
-							.println("Last Name: " + objStudent.getLastName());
-					System.out.println("Branch : " + objStudent.getBranch());
-					System.out
-							.println("---------------------------------------------");
+					objectService.addNewStudent(queue);
+					break;
 				}
-				break;
-			}
-			default: {
-				System.out.println("Wrong Choice!!");
-			}
 
+				case 2: {
+					List<Student> list = objectService.printStudent();
+					Iterator<Student> itr = list.iterator();
+					while (itr.hasNext()) {
+
+						Student objStudent = itr.next();
+						System.out.println("Roll Number: "
+								+ objStudent.getRollNumber());
+						System.out.println("First Name: "
+								+ objStudent.getFirstName());
+						System.out.println("Last Name: "
+								+ objStudent.getLastName());
+						System.out
+								.println("Branch : " + objStudent.getBranch());
+						System.out
+								.println("---------------------------------------------");
+					}
+					break;
+				}
+				default: {
+					System.out.println("Wrong Choice!!");
+				}
+
+				}
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
 	}
 
 }
